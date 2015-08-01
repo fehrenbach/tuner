bd05da06ef6de88f15f326b81318e851b0ce1f5e
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2015-08-01 11:02:37 -0700

    Remove playback code

--- src/main.rs
+++ src/main.rs
@@ -180,65 +180,11 @@ fn autocorrelate(data: &[Sample]) -> Phase {
     min_phase
 }
 
-// Frequency range:
-// Cello & Guitar open strings
-// + some room downwards for drop tunings (1 tone?)
-// + some room both directions for being off (semitone?)
-
-// Actually, autocorrelation is terrible with octaves anyways, so why not
-// just go over one octave? Preferably the lowest, I think.
-
 fn main() {
-    // make_noise();
     let config = default_config();
 
-    {
-        let pcm = PCM::open(&*config.card, Direction::Capture, false).unwrap();
+    let pcm = PCM::open(&*config.card, Direction::Capture, false).unwrap();
 
-        let hwp = HwParams::any(&pcm).unwrap();
-        hwp.set_channels(1).unwrap();
-        hwp.set_rate(SAMPLE_RATE, 0).unwrap();
-        hwp.set_format(Format::s16()).unwrap();
-        hwp.set_access(Access::RWInterleaved).unwrap();
-        pcm.hw_params(&hwp).unwrap();
-        let io = pcm.io_i16().unwrap();
-
-        pcm.prepare().unwrap();
-        loop {
-            let mut data = [0i16; SAMPLES];
-            assert_eq!(io.readi(&mut data).unwrap(), SAMPLES);
-            let phase = autocorrelate(&data);
-            // VT100 escape magic to clear the current line and reset the cursor
-            print!("\x1B[2K\r");
-            print!("phase: {}, freq: {:.3}, pitch: {:.3}, note: {}", phase, SAMPLE_RATE as f64 / phase as f64, frequency(&config, phase as f64), pprint_pitch(frequency(&config, phase as f64).round() as isize));
-            std::io::stdout().flush().unwrap();
-        }
-    }
-    const n :usize = 100;
-    let mut buf = [0i16; n*1024];
-
-    {
-        let pcm = PCM::open(&*config.card, Direction::Capture, false).unwrap();
-
-        let hwp = HwParams::any(&pcm).unwrap();
-        hwp.set_channels(1).unwrap();
-        hwp.set_rate(SAMPLE_RATE, 0).unwrap();
-        hwp.set_format(Format::s16()).unwrap();
-        hwp.set_access(Access::RWInterleaved).unwrap();
-        pcm.hw_params(&hwp).unwrap();
-        let io = pcm.io_i16().unwrap();
-
-        pcm.prepare().unwrap();
-        assert_eq!(io.readi(&mut buf[..]).unwrap(), n*1024); //2*44100);//, 1024);
-
-        pcm.drain().unwrap();
-    }
-
-    println!("playback!");
-
-    let pcm = PCM::open(&*config.card, Direction::Playback, false).unwrap();
-
-    // Set hardware parameters: 44100 Hz / Mono / 16 bit
     let hwp = HwParams::any(&pcm).unwrap();
     hwp.set_channels(1).unwrap();
     hwp.set_rate(SAMPLE_RATE, 0).unwrap();
@@ -247,43 +193,14 @@ fn main() {
     pcm.hw_params(&hwp).unwrap();
     let io = pcm.io_i16().unwrap();
 
-    // Play recording back for 2 seconds.
-    //for _ in 0..2*44100/1024 {
-        assert_eq!(io.writei(&buf[..]).unwrap(), n*1024);//2*44100);//, 1024);
-    //}
-
-    // In case the buffer was larger than 2 seconds, start the stream manually.
-    if pcm.state() != State::Running { pcm.start().unwrap() };
-    // Wait for the stream to finish playback.
-    pcm.drain().unwrap();
-}
-
-fn make_noise() {
-    // Open default playback device
-    let pcm = PCM::open(&*CString::new("default").unwrap(), Direction::Playback, false).unwrap();
-
-    // Set hardware parameters: 44100 Hz / Mono / 16 bit
-    let hwp = HwParams::any(&pcm).unwrap();
-    hwp.set_channels(1).unwrap();
-    hwp.set_rate(44100, 0).unwrap();
-    hwp.set_format(Format::s16()).unwrap();
-    hwp.set_access(Access::RWInterleaved).unwrap();
-    pcm.hw_params(&hwp).unwrap();
-    let io = pcm.io_i16().unwrap();
-
-    // Make a sine wave
-    let mut buf = [0i16; 1024];
-    for (i, a) in buf.iter_mut().enumerate() {
-        *a = ((i as f32 * 2.0 * ::std::f32::consts::PI / 128.0).sin() * 8192.0) as i16
+    pcm.prepare().unwrap();
+    loop {
+        let mut data = [0i16; SAMPLES];
+        assert_eq!(io.readi(&mut data).unwrap(), SAMPLES);
+        let phase = autocorrelate(&data);
+        // VT100 escape magic to clear the current line and reset the cursor
+        print!("\x1B[2K\r");
+        print!("phase: {}, freq: {:.3}, pitch: {:.3}, note: {}", phase, SAMPLE_RATE as f64 / phase as f64, frequency(&config, phase as f64), pprint_pitch(frequency(&config, phase as f64).round() as isize));
+        std::io::stdout().flush().unwrap();
     }
-
-    // Play it back for 2 seconds.
-    for _ in 0..2*44100/1024 {
-        assert_eq!(io.writei(&buf[..]).unwrap(), 1024);
-    }
-
-    // In case the buffer was larger than 2 seconds, start the stream manually.
-    if pcm.state() != State::Running { pcm.start().unwrap() };
-    // Wait for the stream to finish playback.
-    pcm.drain().unwrap();
 }
